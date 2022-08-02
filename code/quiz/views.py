3ad039ccea9ec78ceb6a85973a9a8b12d32ab967
Beilong <bt132@duke.edu>
2022-08-02 14:04:52 +0800

    update

--- code/quiz/views.py
+++ code/quiz/views.py
@@ -47,8 +47,6 @@ def assignment(request,user_id):
 
     progress_array, passed_array = ut.get_progress(student.question_set.all(),len(due_dict))
 
-
-
     set=[101,201,301,401,501,601,701]
     context['array']=list(zip( due_dict.keys(), due_dict.values(), progress_array, set,due_date , passed_array))
     return render(request,'quiz/assignment.html',context)
