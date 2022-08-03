140ebdc159b96990d846cab45b639b1a5f453a31
Brooks <jiang.long@gmail.com>
2022-08-03 20:39:45 +0800

    ADD: echo-sever template for external update of DB

---
+++ code/echo-server.py
@@ -0,0 +1,43 @@
+# echo-server.py
+
+import socket, sys
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+
+# use 'telnet hostname PORT' to connect  
+s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+while True:
+    try : 
+        s.bind((HOST, PORT))
+        print('connected on port %s' % PORT)
+        
+        break
+    except Exception as e:
+        PORT = PORT - 1 
+        print(e)
+        pass
+    pass
+while True:
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            
+            #----------------------------------
+            # replace this with the update code
+            #----------------------------------
+            
+            if not data:
+                break
+            print(data)
+            conn.sendall(b'repolab-quiz server: DB updated\n')
+            break
+        pass
+    conn.close()
+    
+    pass
+
